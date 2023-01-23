13bfb8d96cb13c5f07c3bc0a613b77c968e73a45
abbigailshegone <112713146+abbigailshegonee@users.noreply.github.com>
2023-01-23 15:10:40 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,8 @@
+num1 = 475
+num2 = 821
+num3 = 369
+num4 = 562
+sum = num1 + num2 + num3 + num4
+average = round((sum / 4), 2)
+print("The sum of the four numbers is", sum)
+print("The average of the four numbers is", average)
---
+++ prog54c.py
@@ -0,0 +1,7 @@
+pi = 3.14159
+r = float(input("Enter radius: "))
+circum = round((2 * pi * r), 3)
+area = round((pi * (r ** 2)), 3)
+print("The radius of the circle =", r)
+print("The area of the circle =", area)
+print("The circumfrence of the circle =", circum)

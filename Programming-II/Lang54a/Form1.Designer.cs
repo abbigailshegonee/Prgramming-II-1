e69317f166471e9a83ec5b9c68fc8fe43294c655
abbigailshegonee <112713146+abbigailshegonee@users.noreply.github.com>
2023-04-12 08:06:03 -0500

    update

---
+++ Programming-II/Lang54a/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Lang54a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.lblGallons = new System.Windows.Forms.Label();
+            this.lblMpg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(189)))), ((int)(((byte)(38)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.No;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Book", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(81)))), ((int)(((byte)(15)))));
+            this.button1.Location = new System.Drawing.Point(26, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(189)))), ((int)(((byte)(38)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.No;
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Book", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(81)))), ((int)(((byte)(15)))));
+            this.button2.Location = new System.Drawing.Point(222, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(189)))), ((int)(((byte)(38)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.No;
+            this.button3.Font = new System.Drawing.Font("Franklin Gothic Book", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(81)))), ((int)(((byte)(15)))));
+            this.button3.Location = new System.Drawing.Point(222, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(255)))), ((int)(((byte)(189)))));
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.comboBox1.Font = new System.Drawing.Font("Franklin Gothic Book", 19.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.OliveDrab;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1970 VW Bug",
+            "1979 Firebird",
+            "1980 Subaru",
+            "1975 Cutlass"});
+            this.comboBox1.Location = new System.Drawing.Point(26, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 41);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "1970 VW Bug";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.OliveDrab;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Ivory;
+            this.label1.Location = new System.Drawing.Point(26, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 41);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Miles :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.OliveDrab;
+            this.label2.Cursor = System.Windows.Forms.Cursors.No;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Ivory;
+            this.label2.Location = new System.Drawing.Point(26, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 41);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Gallons :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.OliveDrab;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Ivory;
+            this.label3.Location = new System.Drawing.Point(26, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Miles Per Gallon :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.BackColor = System.Drawing.Color.YellowGreen;
+            this.lblMiles.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblMiles.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiles.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblMiles.Location = new System.Drawing.Point(222, 145);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(133, 41);
+            this.lblMiles.TabIndex = 7;
+            this.lblMiles.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblGallons
+            // 
+            this.lblGallons.BackColor = System.Drawing.Color.YellowGreen;
+            this.lblGallons.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblGallons.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGallons.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblGallons.Location = new System.Drawing.Point(222, 193);
+            this.lblGallons.Name = "lblGallons";
+            this.lblGallons.Size = new System.Drawing.Size(133, 41);
+            this.lblGallons.TabIndex = 8;
+            this.lblGallons.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblMpg
+            // 
+            this.lblMpg.BackColor = System.Drawing.Color.YellowGreen;
+            this.lblMpg.Cursor = System.Windows.Forms.Cursors.No;
+            this.lblMpg.Font = new System.Drawing.Font("Franklin Gothic Book", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMpg.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.lblMpg.Location = new System.Drawing.Point(222, 241);
+            this.lblMpg.Name = "lblMpg";
+            this.lblMpg.Size = new System.Drawing.Size(133, 41);
+            this.lblMpg.TabIndex = 9;
+            this.lblMpg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(383, 309);
+            this.Controls.Add(this.lblMpg);
+            this.Controls.Add(this.lblGallons);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.Label lblGallons;
+        private System.Windows.Forms.Label lblMpg;
+
+    }
+}
+
---
+++ Programming-II/Lang54a/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lang54a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int miles = 0;
+            int gallons = 0;
+            double mpg = 0;
+            if (comboBox1.Text == "1970 VW Bug") {
+                miles = 286;
+                gallons = 9;
+            } 
+            else if (comboBox1.Text == "1979 Firebird") {
+                miles = 412;
+                gallons = 40;
+            }
+            else if (comboBox1.Text == "1980 Subaru") {
+                miles = 361;
+                gallons = 18;
+            }
+            else if (comboBox1.Text == "1975 Cutlass") {
+                miles = 161;
+                gallons = 11;
+            }
+            else {
+                MessageBox.Show("Ivalid car selection!");
+                return; //Immediately exit the function, no 0/0
+            }
+            mpg = (double)miles / gallons;
+            mpg = Math.Round(mpg, 3);
+            lblMiles.Text = " " + miles.ToString();
+            lblGallons.Text = " " + gallons.ToString();
+            lblMpg.Text = " " + mpg.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblMiles.Text = "";
+            lblGallons.Text = "";
+            lblMpg.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
